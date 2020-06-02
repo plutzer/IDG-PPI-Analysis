31f441e887a49d72d5807862c887227e87c8a05d
DennisGoldfarb <d.goldfarb@wustl.edu>
2020-06-02 13:44:01 -0500

    SAINT and CompPASS scoring

--- protein_groups.py
+++ protein_groups.py
@@ -1,42 +1,62 @@
 import pandas as pd
 import os
+import csv
+import re
+from collections import defaultdict
+from math import sqrt, log2, pow
+
+def get_quant_col_prefix(quantification):
+    if quantification == "LFQ":
+        return "LFQ intensity "
+    elif quantification == "intensity":
+        return "Intensity "
+    elif quantification == "spc":
+        return "MS/MS count "
+
 
 class ProteinGroups:
-    def __init__(self, experimental_design, file_path, quantification):
+    def __init__(self, experimental_design, file_path, quantification_saint, quantification_comppass):
         print("\nParsing MaxQuant proteinGroups: " + file_path)
 
-        self.quantification = quantification
         self.experimental_design = experimental_design
+        self.quantification_saint = quantification_saint
+        self.quantification_comppass = quantification_comppass
         self.data = pd.read_csv(file_path, sep="\t", quotechar="'")
 
-        if quantification == "LFQ":
-            quant_col_prefix = "LFQ intensity "
-        elif quantification == "intensity":
-            quant_col_prefix = "Intensity "
-        elif quantification == "spc":
-            quant_col_prefix = "MS/MS count "
+        self.quant_col_prefix_saint = get_quant_col_prefix(quantification_saint)
+        self.quant_col_prefix_comppass = get_quant_col_prefix(quantification_comppass)
 
-        self.quant_cols = []
+        self.quant_cols_saint = []
+        self.quant_cols_comppass = []
         self.bait_cols = []
 
         # make sure experimental design matches the data
         for col in self.data.columns:
-            if col.startswith(quant_col_prefix):
-                exp_name = col.split(quant_col_prefix)[-1]
+            if col.startswith(self.quant_col_prefix_saint):
+                exp_name = col.split(self.quant_col_prefix_saint)[-1]
                 if exp_name not in self.experimental_design.name2experiment:
                     print("WARNING: Experiment found in proteinGroups,"
                           " but missing from experimental design:", exp_name)
                 else:
-                    if quantification == "spc":
+                    if quantification_saint == "spc":
                         self.data[col] = self.data[col].astype(int)
                     else:
                         self.data[col] = self.data[col].astype(float)
-                    self.quant_cols.append(col)
+                    self.quant_cols_saint.append(col)
                     if experimental_design.name2experiment[exp_name].attributes["Type"] == "T":
                         self.bait_cols.append(col)
+            if col.startswith(self.quant_col_prefix_comppass):
+                exp_name = col.split(self.quant_col_prefix_comppass)[-1]
+                if exp_name in self.experimental_design.name2experiment:
+                    if quantification_comppass == "spc":
+                        self.data[col] = self.data[col].astype(int)
+                    else:
+                        self.data[col] = self.data[col].astype(float)
+                    self.quant_cols_comppass.append(col)
 
         for name in self.experimental_design.name2experiment:
-            if (quant_col_prefix + name) not in self.quant_cols:
+            if (self.quant_col_prefix_saint + name) not in self.quant_cols_saint or \
+                    (self.quant_col_prefix_comppass + name) not in self.quant_cols_comppass:
                 print("ERROR: Experiment found in experimental design, but missing from proteinGroups:", name)
                 exit(1)
 
@@ -62,10 +82,11 @@ class ProteinGroups:
         for index in self.data.index:
             split_prots = self.data.loc[index, "Short protein IDs"].split(";")
             split_genes = self.data.loc[index, "Short Gene names"].split(";")
+
             if len(split_prots) > 3:
-                self.data.loc[index, "Short protein IDs"] = ";".join(split_prots[0:3]) + ";+" + str(len(split_prots)-3)
+                self.data.loc[index, "Short protein IDs"] = re.sub(" ", "-", ";".join(split_prots[0:3]) + ";+" + str(len(split_prots)-3))
             if len(split_genes) > 3:
-                self.data.loc[index, "Short Gene names"] = ";".join(split_genes[0:3]) + ";+" + str(len(split_genes)-3)
+                self.data.loc[index, "Short Gene names"] = re.sub(" ", "-", ";".join(split_genes[0:3]) + ";+" + str(len(split_genes)-3))
 
     def impute(self):
         1
@@ -74,20 +95,184 @@ class ProteinGroups:
         1
 
     def write_prey_file(self, out_path):
-        self.data[["Short protein IDs", "Sequence length", "Short Gene names"]].to_csv(out_path, index=False, sep="\t", header=False)
+        if self.quantification_saint == "spc":
+            self.data[["Short protein IDs", "Sequence length", "Short Gene names"]].to_csv(out_path, index=False,
+                                                                                           sep="\t", header=False)
+        else:
+            self.data[["Short protein IDs", "Short Gene names"]].to_csv(out_path, index=False, sep="\t", header=False)
 
     def write_bait_file(self, out_path):
-        1
+        with open(out_path, 'w') as csvfile:
+            writer = csv.writer(csvfile, delimiter='\t')
+            for e in self.experimental_design.name2experiment.values():
+                writer.writerow([e.attributes["Experiment Name"], e.attributes["Bait"], e.attributes["Type"]])
 
     def write_interaction_file(self, out_path):
-        1
+        with open(out_path, 'w') as csvfile:
+            writer = csv.writer(csvfile, delimiter='\t')
+            for index, row in self.data.iterrows():
+                for quant_col in self.quant_cols_saint:
+                    exp_name = quant_col.split(self.quant_col_prefix_saint)[-1]
+                    writer.writerow([exp_name,
+                                    self.experimental_design.name2experiment[exp_name].attributes["Bait"],
+                                    row["Short protein IDs"],
+                                    row[quant_col]])
 
-    def to_SAINTv2(self, out_path):
+    def to_SAINT(self, out_path):
         self.write_prey_file(os.path.join(out_path, "prey.txt"))
-        self.write_bait_file(out_path)
-        self.write_interaction_file(out_path)
+        self.write_bait_file(os.path.join(out_path, "bait.txt"))
+        self.write_interaction_file(os.path.join(out_path, "interaction.txt"))
 
-    def to_SAINTexpress(self, out_path):
-        1
+    def to_SAINTq(self, out_path, normalize_control, compress_n_ctrl, compress_n_rep):
+        self.write_toSAINTq_intensity_file(os.path.join(out_path, "intensities.txt"))
+        self.write_toSAINTq_param_file("intensities.txt",
+                                       os.path.join(out_path, "params.txt"),
+                                       normalize_control, compress_n_ctrl, compress_n_rep)
+
+    def write_toSAINTq_intensity_file(self, out_path):
+        with open(out_path, 'w') as csvfile:
+            writer = csv.writer(csvfile, delimiter='\t', lineterminator='\n')
+
+            for index, row in self.data.iterrows():
+                if index == 0:
+                    header1 = [""]
+                    header2 = [""]
+                    header3 = ["Protein"]
+                    for quant_col in self.quant_cols_saint:
+                        exp_name = quant_col.split(self.quant_col_prefix_saint)[-1]
+                        header1.append(self.experimental_design.name2experiment[exp_name].attributes["Type"])
+                        header2.append(self.experimental_design.name2experiment[exp_name].attributes["Bait"])
+                        header3.append(exp_name)
+
+                    writer.writerow(header1)
+                    writer.writerow(header2)
+                    writer.writerow(header3)
+
+                quant_row = [row["Short protein IDs"]]
+                for quant_col in self.quant_cols_saint:
+                    quant_row.append(row[quant_col])
+                writer.writerow(quant_row)
+
+    def write_toSAINTq_param_file(self, intensities_path, out_path, normalize_control, compress_n_ctrl, compress_n_rep):
+        outfile = open(out_path, 'w')
+
+        outfile.write("input_filename=" + intensities_path + "\n")
+
+        outfile.write("input_level=protein\n")
+
+        outfile.write("protein_colname=Protein\n")
+
+        outfile.write("normalize_control=")
+        if normalize_control==1:
+            outfile.write("true\n")
+        else:
+            outfile.write("false\n")
+
+        outfile.write("compress_n_ctrl=")
+        outfile.write(str(compress_n_ctrl) + "\n")
+
+        outfile.write("compress_n_rep=")
+        outfile.write(str(compress_n_rep) + "\n")
+        outfile.close()
+
+    def calc_CompPASS(self):
+        # num baits
+        bait2protID = dict()
+        for e in self.experimental_design.name2experiment.values():
+            bait2protID[e.attributes["Bait"]] = e.attributes["Bait ID"]
+        k = len(bait2protID)
+
+        # for each prey, bait: compute total quant, replicate count, average quant
+        prey2bait2tot = defaultdict(dict)
+        prey2bait2rep = defaultdict(dict)
+        prey2avg = defaultdict(float)
+        for index, row in self.data.iterrows():
+            prey = row["Short protein IDs"]
+            for e in self.experimental_design.name2experiment.values():
+                bait = e.attributes["Bait"]
+                exp_name = e.attributes["Experiment Name"]
+
+                if bait not in prey2bait2tot[prey]:
+                    prey2bait2tot[prey][bait] = 0
+                    prey2bait2rep[prey][bait] = 0
+                if self.quantification_comppass == "spc":
+                    prey2bait2tot[prey][bait] += row[self.quant_col_prefix_comppass + exp_name]
+                else:
+                    prey2bait2tot[prey][bait] += log2(1+row[self.quant_col_prefix_comppass + exp_name])
+
+                if row[self.quant_col_prefix_comppass + exp_name] > 0:
+                    prey2bait2rep[prey][bait] += 1
+
+                if self.quantification_comppass == "spc":
+                    prey2avg[prey] += row[self.quant_col_prefix_comppass + exp_name] / len(self.experimental_design.name2experiment)
+                else:
+                    prey2avg[prey] += log2(1+row[self.quant_col_prefix_comppass + exp_name]) / len(self.experimental_design.name2experiment)
+
+        # for each prey: compute frequency
+        prey2freq = dict()
+        for prey in prey2bait2tot:
+            tot_bait = 0
+            for bait in prey2bait2tot[prey]:
+                if prey2bait2tot[prey][bait] > 0 and prey != bait2protID[bait]:
+                    tot_bait += 1
+            prey2freq[prey] = tot_bait
+
+        # for each prey: compute std
+        prey2stdev = defaultdict(float)
+        prey2W = dict()
+        for index, row in self.data.iterrows():
+            prey = row["Short protein IDs"]
+            for e in self.experimental_design.name2experiment.values():
+                exp_name = e.attributes["Experiment Name"]
+                if self.quantification_comppass == "spc":
+                    prey2stdev[prey] += pow(row[self.quant_col_prefix_comppass + exp_name] - prey2avg[prey], 2)
+                else:
+                    prey2stdev[prey] += pow(log2(1 + row[self.quant_col_prefix_comppass + exp_name]) - prey2avg[prey], 2)
+
+                prey2stdev[prey] = sqrt(prey2stdev[prey] / len(self.experimental_design.name2experiment))
+
+            if prey2avg[prey] > 0:
+                prey2W[prey] = max(1.0, prey2stdev[prey] / prey2avg[prey])
+            else:
+                prey2W[prey] = 0.0
+
+        # calculate WD-score
+        prey2bait2scores = defaultdict(dict)
+        for index, row in self.data.iterrows():
+            prey = row["Short protein IDs"]
+
+            for bait in bait2protID:
+                if prey2avg[prey] > 0:
+                    wd = sqrt(pow(((k / max(1,prey2freq[prey])) * prey2W[prey]), prey2bait2rep[prey][bait]) * prey2bait2tot[prey][bait])
+                    d = sqrt(pow((k / max(1,prey2freq[prey])), prey2bait2rep[prey][bait]) * prey2bait2tot[prey][bait])
+                else:
+                    wd = 0.0
+                    d = 0.0
+
+                prey2bait2scores[prey][bait] = [str(wd), str(d), str(prey2freq[prey])]
+
+        return prey2bait2scores
+
+    def align_scores(self, saint_path, prey2bait2comppass, out_path):
+        with open(saint_path, encoding='utf-8-sig') as csvfile:
+            reader = csv.reader(csvfile, delimiter="\t")
+            header = next(reader)
+
+            with open(out_path, 'w') as csvfile_out:
+                writer = csv.writer(csvfile_out, delimiter='\t')
+
+                new_header = header
+                new_header.extend(["WD-score", "D-score", "frequency"])
+                writer.writerow(new_header)
+                for row in reader:
+                    prey = row[1]
+                    bait = row[0]
+                    new_row = row
+
+                    new_row.extend(prey2bait2comppass[prey][bait])
+                    writer.writerow(new_row)
+
+
+
 
 
--- score_APMS.py
+++ score_APMS.py
@@ -2,6 +2,7 @@
 import sys
 import os
 import argparse
+import subprocess
 from experimental_design import ExperimentalDesign
 from protein_groups import ProteinGroups
 
@@ -10,6 +11,7 @@ SAINT_v2_INT_DIR = SAINT_DIR + "saint-int-ctrl"
 SAINT_v2_SPC_DIR = SAINT_DIR + "saint-spc-ctrl"
 SAINT_EXPRESS_INT_DIR = SAINT_DIR + "SAINTexpress-int"
 SAINT_EXPRESS_SPC_DIR = SAINT_DIR + "SAINTexpress-spc"
+SAINT_Q_DIR = SAINT_DIR + "SAINTq"
 
 ########################################################################################################################
 # Command line argument parsing
@@ -22,30 +24,39 @@ parser = argparse.ArgumentParser(description=description)
 
 
 # generic arguments
-parser.add_argument("proteinGroups",
-                    help="path to MaxQuant ProteinGroups.txt")
+parser.add_argument("--proteinGroups",
+                    help="path to MaxQuant ProteinGroups.txt",
+                    default="proteinGroups.txt")
 
-parser.add_argument("experimentalDesign",
-                    help="path to experimental design file")
+parser.add_argument("--experimentalDesign",
+                    help="path to experimental design file",
+                    default="ExperimentalDesign.csv")
 
 # output arguments
-parser.add_argument("outputPath",
-                    help="path for the output directory. If it already exists it will be overwritten.")
+parser.add_argument("--outputPath",
+                    help="path for the output directory. If it already exists it will be overwritten.",
+                    default=".")
 
-parser.add_argument("-q", "--quantification",
-                    help="quantification values to use",
+parser.add_argument("-qs", "--quantification-saint",
+                    help="quantification values to use for SAINT",
                     choices=["spc", "LFQ", "intensity"],
-                    default="LFQ")
+                    default="spc")
 
-parser.add_argument("-i", "--imputation",
-                    help="method to impute missing quantification data",
-                    choices=["none", "perseus"],
-                    default="none")
+parser.add_argument("-qc", "--quantification-comppass",
+                    help="quantification values to use for CompPASS",
+                    choices=["spc", "LFQ", "intensity"],
+                    default="spc")
+
+#parser.add_argument("-i", "--imputation",
+#                    help="method to impute missing quantification data",
+#                    choices=["none", "perseus"],
+#                    default="none")
 
 # SAINT arguments
 parser.add_argument("-s", "--SAINT",
                     help="SAINT version to use",
-                    choices=["v2", "express"])
+                    choices=["v2", "express", "q"],
+                    default="express")
 
 parser.add_argument("--nburnin",
                     help="SAINT v2: number of burn-in iterations in MCMC. Default: 2000",
@@ -77,6 +88,29 @@ parser.add_argument("--normalize",
                     choices=["0", "1"],
                     default="1")
 
+# SAINTq arguments
+parser.add_argument("--normalize-control",
+                    help="SAINTq: normalize control intensities by multiplying a constant to all control intensities\n" +
+                         "so that the average observed test intensities is equal to the average control intensities.\n" +
+                         " Default: 0",
+                    choices=["0", "1"],
+                    default="0")
+
+parser.add_argument("--compress-n-ctrl",
+                    help="SAINTq: the number of control baits used in calculations, with priority for baits with\n" +
+                         "greatest intensities. Setting this number to a large number makes the program use all\n" +
+                         "available control data (recommend in cases with at most several controls. Default: 1000",
+                    type=int,
+                    default=1000)
+
+parser.add_argument("--compress-n-rep",
+                    help="SAINTq: the number of test bait replicates used for scoring, with priotiy given to the\n" +
+                         "baits with higher probability scores. If this number is greater than or equal to the number\n" +
+                         "of available replicates, then the scores will use the data from all replicates. Otherwise, \n" +
+                         "the highest scoring replicate scores wil lbe averaged to yield the final probability score.\n" +
+                         "Default: 1000",
+                    type=int,
+                    default=1000)
 
 args = parser.parse_args()
 
@@ -101,41 +135,106 @@ if not os.path.exists(args.experimentalDesign) or not os.path.isfile(args.experi
     print("ERROR: experimental design file not found: " + args.experimentalDesign)
     exit(1)
 
+if args.SAINT == "q":
+    if not os.path.exists(SAINT_Q_DIR):
+        print("ERROR: " + SAINT_Q_DIR + " not found")
+        exit(1)
+
 if args.SAINT == "v2":
-    if args.quantification == "intensity" or args.quantification == "LFQ":
+    if args.quantification_saint == "intensity" or args.quantification_saint == "LFQ":
         if not os.path.exists(SAINT_v2_INT_DIR):
             print("ERROR: " + SAINT_v2_INT_DIR + " not found")
             exit(1)
-    elif args.quantification == "spc":
+    elif args.quantification_saint == "spc":
         if not os.path.exists(SAINT_v2_SPC_DIR):
             print("ERROR: " + SAINT_v2_SPC_DIR + " not found")
             exit(1)
 
 if args.SAINT == "express":
-    if args.quantification == "intensity" or args.quantification == "LFQ":
+    if args.quantification_saint == "intensity" or args.quantification_saint == "LFQ":
         if not os.path.exists(SAINT_EXPRESS_INT_DIR):
             print("ERROR: " + SAINT_EXPRESS_INT_DIR + " not found")
             exit(1)
-    elif args.quantification == "spc":
+    elif args.quantification_saint == "spc":
         if not os.path.exists(SAINT_EXPRESS_SPC_DIR):
             print("ERROR: " + SAINT_EXPRESS_SPC_DIR + " not found")
             exit(1)
 
+if args.SAINT == "q":
+    if args.quantification_saint == "spc":
+        print("ERROR: SAINTq does not support spectral counts.")
+        exit(1)
 
 # confirm they aren't trying to impute spc
-if args.imputation and args.imputation == "1" and args.quantification == "spc":
-    print("ERROR: Imputation of spectral counts is not supported.")
-    exit(1)
+#if args.imputation and args.imputation == "1" and args.quantification == "spc":
+#    print("ERROR: Imputation of spectral counts is not supported.")
+#    exit(1)
 
 # parse experimental design
 experimental_design = ExperimentalDesign(args.experimentalDesign)
 
 # process MaxQuant proteinGroups
-protein_groups = ProteinGroups(experimental_design, args.proteinGroups, args.quantification)
+protein_groups = ProteinGroups(experimental_design, args.proteinGroups,
+                               args.quantification_saint, args.quantification_comppass)
 
 # impute missing values if requested
-if args.imputation == "1":
-    protein_groups.impute()
+#if args.imputation == "1":
+#    protein_groups.impute()
+
+# output SAINT formatted files
+if args.SAINT == "q":
+    protein_groups.to_SAINTq(args.outputPath, args.normalize_control, args.compress_n_ctrl, args.compress_n_rep)
+else:
+    protein_groups.to_SAINT(args.outputPath)
+
+# execute CompPASS
+prey2bait2comppass = protein_groups.calc_CompPASS()
+
+# execute SAINT
+if args.SAINT == "q":
+    SAINT_path = SAINT_Q_DIR
+    print("\nExecuting SAINT:", SAINT_path)
+    p = subprocess.run([SAINT_path,
+                        os.path.join(args.outputPath, "params.txt")])
+
 
 if args.SAINT == "v2":
-    protein_groups.to_SAINTv2(args.outputPath)
+    if args.quantification_saint == "intensity" or args.quantification_saint == "LFQ":
+        SAINT_path = SAINT_v2_INT_DIR
+    else:
+        SAINT_path = SAINT_v2_SPC_DIR
+
+    print("\nExecuting SAINT:", SAINT_path)
+    p = subprocess.run([SAINT_path,
+                        os.path.join(args.outputPath, "interaction.txt"),
+                        os.path.join(args.outputPath, "prey.txt"),
+                        os.path.join(args.outputPath, "bait.txt"),
+                        str(args.nburnin),
+                        str(args.niter),
+                        str(args.lowMode),
+                        str(args.minFold),
+                        str(args.normalize)])
+
+if args.SAINT == "express":
+    if args.quantification_saint == "intensity" or args.quantification_saint == "LFQ":
+        SAINT_path = SAINT_EXPRESS_INT_DIR
+    else:
+        SAINT_path = SAINT_EXPRESS_SPC_DIR
+
+    print("\nExecuting SAINT:", SAINT_path)
+    p = subprocess.run([SAINT_path,
+                        "-L",
+                        "2",
+                        os.path.join(args.outputPath, "interaction.txt"),
+                        os.path.join(args.outputPath, "prey.txt"),
+                        os.path.join(args.outputPath, "bait.txt")])
+
+
+if args.SAINT == "express":
+    protein_groups.align_scores(os.path.join(args.outputPath,  "list.txt"),
+                                prey2bait2comppass,
+                                os.path.join(args.outputPath, "candidates.tsv"))
+else:
+    protein_groups.align_scores(os.path.join(args.outputPath, "RESULT", "unique_interactions"),
+                                prey2bait2comppass,
+                                os.path.join(args.outputPath, "candidates.tsv"))
