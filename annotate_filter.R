da5a1686e7249e63b3137f9418b84e1c395e4051
smarasolo <smaranda@wustl.edu>
2022-02-11 14:25:42 -0600

    Prey-prey output includes both WD scores

--- annotate_filter.R
+++ annotate_filter.R
@@ -19,8 +19,10 @@ library(org.Hs.eg.db)
 library(DarkKinaseTools)
 source("Cytoscape.R")
 source("SiRNA_data.R")
+#source("csnk1g_figures.R")
 
 select <- get(x="select", pos = "package:dplyr")
+#rename <- get(x = "rename", pos = "package:dplyr")
 
 uniprot.mapping <- read_tsv("annotations/uniprot_mapping.tsv.zip")
 
@@ -56,7 +58,8 @@ data <- left_join(data, uniprot.mapping, by=c("Canonical.First.Bait.Uniprot" = "
 #Move and rename columns
 data <- data[,c(1:7, 32:39, 8:31)]
 data <- rename(data, c(Gene_Name.x = "Prey.Gene.Name", Gene_Synonym.x = "Prey.Gene.Synonym", GeneID.x = "Prey.GeneID", First_GeneID.x = "First.Prey.GeneID", 
-                               Gene_Name.y = "Bait.Gene.Name", Gene_Synonym.y = "Bait.Gene.Synonym", GeneID.y = "Bait.GeneID", First_GeneID.y = "First.Bait.GeneID"))
+                       Gene_Name.y = "Bait.Gene.Name", Gene_Synonym.y = "Bait.Gene.Synonym", GeneID.y = "Bait.GeneID", First_GeneID.y = "First.Bait.GeneID"))
+data <- data[,c(1,12,13,2:4,14,15,8,9,5:7,10,11,16:39)]
 
 ################################################################################
 #Annotations
@@ -71,7 +74,7 @@ go <- c("Nucleus","Cytoplasm","Cytoskeleton","Endosome","ER","Extracellular","Go
 for(i in 1:length(go)) {
   goids <- xx[x[i]]
   goslim <- tibble(Prey.GeneID = goids[[1]], Evidence = names(goids[[1]]))
-  goslim <- filter(goslim, "IEA" != Evidence)
+  #goslim <- filter(goslim, "IEA" != Evidence)
   goslim <- (goslim %>% 
                group_by(Prey.GeneID)%>% 
                summarise(n=n()))
@@ -89,12 +92,11 @@ data$GO.Slim <- apply(data, 1, function(x) {
                         x[["Extracellular"]], x[["Golgi"]], x[["Lysosome"]], x[["Mitochondria"]],
                         x[["Peroxisome"]], x[["Plasma_Membrane"]], x[["Vesicles"]], x[["Cell_Junction"]]))], 
         collapse = ";")
-  }
+}
 )
 
 #Priority GO Column: Either matched with bait or from uniprot
 #data$GO.Priority <- apply(data, 1, function(x)){
-  
 #}
 
 #bait.data.filter <- data.filter %>% filter(Bait == mybait)
@@ -107,7 +109,7 @@ data <- left_join(data, all_kinases, by=c("First.Prey.GeneID" = "entrez_id"))
 #Is it a Bait Column
 data$is_Bait <- apply(data, 1, function(x) {
   any(str_detect(unique.Baits, x[["Canonical.First.Prey.Uniprot"]]))
-  }
+}
 )
 
 #Nice bait name column addition by grabbing from prey name if it pairs with is_bait column
@@ -123,7 +125,7 @@ data <- left_join(data, baitTable, by="Bait")
 #BioGrid Annotations
 
 #Biogrid read  in
-biogrid <- read.csv(file = 'C:/Users/smaranda/Documents/SmarandaSolomon/BIOGRID-MV-Physical-4.2.193.tab3.txt', header = TRUE, sep="\t", stringsAsFactors = FALSE) %>%
+biogrid <- read.csv(file = 'C:/Users/smaranda/Documents/SmarandaSolomon/BIOGRID/BIOGRID-MV-Physical-4.4.203.tab3.txt', header = TRUE, sep="\t", stringsAsFactors = FALSE) %>%
   filter(Organism.ID.Interactor.A == 9606 & Organism.ID.Interactor.B == 9606)
 
 #Is In BioGrid (T/F)
@@ -142,6 +144,7 @@ interactions <- biogrid %>%
   select(interactor_min, interactor_max) %>%
   unique()
 
+#CHANGE AS.NUMERIC AND PUT IT ABOVE SO IT GOES FASTER
 data$in_BioGRID <- apply(data, 1, function(x) {
   nrow(filter(interactions, (`interactor_min` == as.numeric(x[["Prey.GeneID"]]) & `interactor_max` == as.numeric(x[["Bait.GeneID"]])) |
                 (`interactor_max` == as.numeric(x[["Prey.GeneID"]]) & `interactor_min` == as.numeric(x[["Bait.GeneID"]]))
@@ -149,14 +152,7 @@ data$in_BioGRID <- apply(data, 1, function(x) {
 }) 
 
 ################################################################################
-#Add SiRNA Data
-
-sirna <- sirna.data(data)
-
-data <- left_join(data, sirna, by = c("Bait.Gene.Name" = "Gene.Symbol"))
-
-################################################################################
-#Filter and Write files
+#Filter SAINT
 
 #Write file with no filter
 write_csv(data, 'output/Annotated_Merge_NO_FILTER.csv')
@@ -164,38 +160,57 @@ write_csv(data, 'output/Annotated_Merge_NO_FILTER.csv')
 #Filter SAINT
 data.filter <- filter(data, BFDR <= 0.05, AvgP >= 0.7)
 
-#Filter CompPASS
-data.filter <- arrange(data.filter, desc(WD))
+################################################################################
+#Add SiRNA Data
 
-#Write file with all experiments
+sirna <- sirna.data()
+
+data.filter <- left_join(data.filter, sirna, by = c("Prey.Gene.Name" = "Gene.Symbol"), copy = TRUE)
+
+#Write file filtered for Saint
 write_csv(data.filter, 'output/Annotated_Merge_Saint_filter.csv')
 
+################################################################################
+#Filter comppass and beyond
+
+#Filter CompPASS
+#Arrange first
+data.filter.comp <- arrange(data.filter, desc(WD))
+
 #Filter by top 5% or top min *KEEPING Everything*
-baits <- unique((data.filter %>% filter(!is.na(BaitGene), is_Bait == TRUE, BaitGene == PreyGene))$Bait)
-all.data.filter <- data.filter[0,]
+baits <- unique((data.filter.comp %>% filter(!is.na(BaitGene), is_Bait == TRUE, BaitGene == PreyGene))$Bait)
+all.data.filter <- data.filter.comp[0,]
 
 #Create separate data tables for prey-prey interactions
 for (mybait in baits) {
+  
+  # CDK2_P24941
+  # CSNK1G1_Q9HCP0
+  # CSNK1G2_P78368
+  # 
+   mybait <- "P24941"
 
-  bait.data.filter <- data.filter %>% filter(Bait == mybait)
-  num.interactors <- min(max(10, nrow(bait.data.filter)*0.05), nrow(bait.data.filter))
-  all.data.filter <- all.data.filter %>% add_row(bait.data.filter[1:num.interactors, ])
+  bait.data.filter <- data.filter.comp %>% filter(Bait == mybait)
+  num.interactors <- min(max(10, nrow(bait.data.filter)*0.05), nrow(data.filter.comp))
+  bait.data.filter.comp <- bait.data.filter[1:num.interactors, ]
+  all.data.filter <- all.data.filter %>% add_row(bait.data.filter.comp)
   
   #Filtering for interactions
-  prey.prey.inter <- filter(interactions, (`interactor_min` %in%  bait.data.filter$First.Prey.GeneID), (`interactor_max` %in%  bait.data.filter$First.Prey.GeneID)) %>%
+  prey.prey.inter <- filter(interactions, (`interactor_min` %in%  bait.data.filter.comp$First.Prey.GeneID), (`interactor_max` %in%  bait.data.filter.comp$First.Prey.GeneID)) %>%
     rename(c(interactor_min = "Prey.1.Entrez.ID", interactor_max = "Prey.2.Entrez.ID")) %>% #Changing column names
      mutate(Prey.1.Entrez.ID = as.character(Prey.1.Entrez.ID), 
             Prey.2.Entrez.ID = as.character(Prey.2.Entrez.ID)) #Changing data type from double to character to be left_joined with all.data.filter
   
   #Left_joining table with prey 1 and adding Uniprot and Nice Prey name columns
-   prey.prey.join <- left_join(prey.prey.inter, bait.data.filter, by=c("Prey.1.Entrez.ID" = "Prey.GeneID"))%>%
+   prey.prey.join <- left_join(prey.prey.inter, bait.data.filter.comp, by=c("Prey.1.Entrez.ID" = "Prey.GeneID"))%>%
      select(Prey.1.Entrez.ID, Prey.2.Entrez.ID, Canonical.First.Prey.Uniprot, Prey.Gene.Name, WD) %>%
      rename(c(Canonical.First.Prey.Uniprot = "Prey.1.Uniprot", Prey.Gene.Name = "Prey.1.Gene.Name", WD = "WD.1"))
 
    #Left_joining table with prey 2 and adding Uniprot and Nice Prey name columns
-   prey.prey.final <- left_join(prey.prey.join, bait.data.filter, by=c("Prey.2.Entrez.ID" = "Prey.GeneID")) %>%
-     select(Prey.1.Gene.Name, Prey.1.Uniprot, Prey.1.Entrez.ID, Prey.Gene.Name, Canonical.First.Prey.Uniprot, Prey.2.Entrez.ID, WD.1) %>%
-     rename(c(Canonical.First.Prey.Uniprot = "Prey.2.Uniprot", Prey.Gene.Name = "Prey.2.Gene.Name"))
+   prey.prey.final <- left_join(prey.prey.join, bait.data.filter.comp, by=c("Prey.2.Entrez.ID" = "Prey.GeneID")) %>%
+     select(Prey.1.Gene.Name, Prey.1.Uniprot, Prey.1.Entrez.ID, WD.1, Prey.Gene.Name, Canonical.First.Prey.Uniprot, Prey.2.Entrez.ID, WD) %>%
+     rename(c(Canonical.First.Prey.Uniprot = "Prey.2.Uniprot", Prey.Gene.Name = "Prey.2.Gene.Name", WD = "WD.2")) %>%
+     unique()
 
    #Add validation column (from Biogrid in this case) if dataframe has data in it
     if(nrow(prey.prey.final) != 0){
@@ -211,11 +226,11 @@ write_csv(all.data.filter, 'output/Annotated_Merge_All_filtered.csv')
 
 ################################################################################
 #Filter by top 5% or top min *For DKK - no controls *
-baits.dkk <- unique((data.filter %>% filter(!is.na(BaitGene), is_Bait == TRUE, BaitGene == PreyGene, !is.na(class_2019)))$Bait)
-all.data.dkk <- data.filter[0,]
+baits.dkk <- unique((data.filter.comp %>% filter(!is.na(BaitGene), is_Bait == TRUE, BaitGene == PreyGene, !is.na(class_2019)))$Bait)
+all.data.dkk <- data.filter.comp[0,]
 
 for (mybait in baits.dkk) {
-  bait.data.filter <- data.filter %>% filter(Bait == mybait)
+  bait.data.filter <- data.filter.comp %>% filter(Bait == mybait)
   num.interactors <- min(max(10, nrow(bait.data.filter)*0.05), nrow(bait.data.filter))
   all.data.dkk <- all.data.dkk %>% add_row(bait.data.filter[1:num.interactors, ])
 }
